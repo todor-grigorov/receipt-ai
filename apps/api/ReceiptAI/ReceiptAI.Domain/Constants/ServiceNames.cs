577da86c5ce0aa86df7edc7ecf6806bd87c7777c
todor-grigorov <t.grigorov99@gmail.com>
2026-06-03 23:52:39 +0200

    feat: add constants in the Domain project

---
+++ apps/api/ReceiptAI/ReceiptAI.Domain/Constants/ServiceNames.cs
@@ -0,0 +1,8 @@
+﻿namespace ReceiptAI.Domain.Constants
+{
+    public static class ServiceNames
+    {
+        public const string Api = "aspnet-api";
+        public const string AzureFunction = "azure-function";
+    }
+}
