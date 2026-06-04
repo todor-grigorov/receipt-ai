a1cbd048c991c243e6b20ea245e3f8a9fd468027
todor-grigorov <t.grigorov99@gmail.com>
2026-06-04 23:47:35 +0200

    feat: ServiceExtensions and register implemented services and repositories

---
+++ apps/api/ReceiptAI/ReceiptAI.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceiptAI.Application.Interfaces.Repositories;
+using ReceiptAI.Application.Interfaces.Services;
+using ReceiptAI.Application.Services;
+using ReceiptAI.Infrastructure.Persistence;
+using ReceiptAI.Infrastructure.Persistence.Repositories;
+
+namespace ReceiptAI.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithExposedHeaders("X-Pagination"));
+            });
+        }
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<AppDbContext>(opts =>
+                opts.UseNpgsql(configuration.GetConnectionString("postgresConnection")));
+
+        public static void ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
+
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IJobService, JobService>();
+            services.AddScoped<IReceiptService, ReceiptService>();
+            services.AddScoped<IAuditService, AuditService>();
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new()
+                {
+                    Title = "ReceiptAI API",
+                    Version = "v1",
+                    Description = "Async receipt processing with Gemini 2.5 Flash"
+                });
+            });
+        }
+    }
+}
