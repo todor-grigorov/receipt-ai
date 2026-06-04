ab5376e08451cc9cd30560f43db6a7cd8d13e57e
todor-grigorov <t.grigorov99@gmail.com>
2026-06-04 14:26:38 +0200

    feat: add IRepositoryBase interface

---
+++ apps/api/ReceiptAI/ReceiptAI.Application/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace ReceiptAI.Application.Interfaces.Repositories
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll(bool trackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
+        bool trackChanges);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
