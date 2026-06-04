f24ab1708c57e7a5300103926205c0e760a273da
todor-grigorov <t.grigorov99@gmail.com>
2026-06-04 20:02:42 +0200

    feat: add JobHub in ReceiptAI.Api project

---
+++ apps/api/ReceiptAI/ReceiptAI.Api/Hubs/JobHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ReceiptAI.Application.Interfaces.Hubs;
+
+namespace ReceiptAI.Api.Hubs
+{
+    public class JobHub : Hub<IJobHub>
+    {
+        public async Task SubscribeToJob(string jobId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"job:{jobId}");
+        }
+
+        public async Task UnsubscribeFromJob(string jobId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"job:{jobId}");
+        }
+    }
+}
