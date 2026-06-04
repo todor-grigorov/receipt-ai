05dd9bfe684b4f8ff17b6f94d56a9fa36c462fe0
todor-grigorov <t.grigorov99@gmail.com>
2026-06-04 14:27:58 +0200

    feat: implement RepositoryBase class

---
+++ apps/api/ReceiptAI/ReceiptAI.Infrastructure/Persistence/RepositoryBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace ReceiptAI.Infrastructure.Persistence
+{
+    public abstract class RepositoryBase<T>(AppDbContext context) : IRepositoryBase<T> where T : class
+    {
+        protected readonly AppDbContext Context = context;
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            trackChanges
+                ? Context.Set<T>()
+                : Context.Set<T>().AsNoTracking();
+
+        public IQueryable<T> FindByCondition(
+            Expression<Func<T, bool>> expression,
+            bool trackChanges) =>
+            trackChanges
+                ? Context.Set<T>().Where(expression)
+                : Context.Set<T>().Where(expression).AsNoTracking();
+
+        public void Create(T entity) => Context.Set<T>().Add(entity);
+        public void Update(T entity) => Context.Set<T>().Update(entity);
+        public void Delete(T entity) => Context.Set<T>().Remove(entity);
+    }
+}
