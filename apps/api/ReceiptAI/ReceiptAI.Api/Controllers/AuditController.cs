b0b714938e1cd2d1c7c70657df56df3e72635717
todor-grigorov <t.grigorov99@gmail.com>
2026-06-05 15:26:31 +0200

    feat: add AuditController

---
+++ apps/api/ReceiptAI/ReceiptAI.Api/Controllers/AuditController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ReceiptAI.Application.DataTransferObjects.Responses;
+using ReceiptAI.Application.Interfaces.Services;
+
+namespace ReceiptAI.Api.Controllers
+{
+    [ApiController]
+    [Route("api/audit")]
+    public class AuditController(
+    IAuditService auditService,
+    IMapper mapper) : ControllerBase
+    {
+        [HttpGet("{correlationId:guid}")]
+        [ProducesResponseType(typeof(IEnumerable<AuditLogResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAuditTrail(
+            Guid correlationId,
+            CancellationToken ct)
+        {
+            var trail = await auditService.GetTrailAsync(correlationId, ct);
+            var response = mapper.Map<IEnumerable<AuditLogResponse>>(trail);
+            return Ok(response);
+        }
+    }
+}
