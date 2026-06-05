73931c928eac3653b8d6508260846a3730f0a626
todor-grigorov <t.grigorov99@gmail.com>
2026-06-05 15:02:48 +0200

    feat: add AppDbContextFactory in ReceiptAI.Api project

---
+++ apps/api/ReceiptAI/ReceiptAI.Api/ContextFactory/AppDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ReceiptAI.Infrastructure.Persistence;
+
+
+namespace ReceiptAI.Api.ContextFactory
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddUserSecrets<AppDbContextFactory>()
+                .Build();
+
+            var connectionString = configuration
+                .GetConnectionString("postgresConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
+                .UseNpgsql(connectionString,
+                    b => b.MigrationsAssembly(
+                        typeof(AppDbContext).Assembly.FullName));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
