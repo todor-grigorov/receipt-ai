c80d67f84925e9bc3dee1383f257128616decb85
todor-grigorov <t.grigorov99@gmail.com>
2026-06-05 15:41:08 +0200

    feat: add CurrentUserService in the ReceiptAI.Api

---
+++ apps/api/ReceiptAI/ReceiptAI.Api/Services/CurrentUserService.cs
@@ -0,0 +1,18 @@
+﻿using ReceiptAI.Application.Interfaces.Services;
+using System.Security.Claims;
+
+namespace ReceiptAI.Api.Services
+{
+    public class CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    : ICurrentUserService
+    {
+        public string UserId => httpContextAccessor.HttpContext?.User
+            .FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty;
+
+        public string Email => httpContextAccessor.HttpContext?.User
+            .FindFirstValue(ClaimTypes.Email) ?? string.Empty;
+
+        public bool IsAuthenticated => httpContextAccessor.HttpContext?.User
+            .Identity?.IsAuthenticated ?? false;
+    }
+}
