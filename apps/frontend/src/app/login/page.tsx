8c33d5d9c32feda3bbe0954b6137ea91b1c03063
todor-grigorov <t.grigorov99@gmail.com>
2026-06-15 20:54:31 +0200

    feat: add signIn functionality in Login page

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { signIn } from "@/lib/auth";
 import MicrosoftLogo from "@/components/MicrosoftLogo";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,26 +10,28 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import React from "react";
 
 const Login = () => {
   return (
-    <div className="w-full h-full bg-[#F9FAFB] flex justify-center items-center">
+    <div className="w-full min-h-screen bg-[#F9FAFB] flex justify-center items-center">
       <Card
         style={{ boxShadow: "0 4px 24px rgba(0,0,0,0.08)" }}
         className="w-md h-fit py-12 shadow-md"
       >
-        <CardHeader className="flex flex-col items-center justufy-center p-0">
+        <CardHeader className="flex flex-col items-center justify-center p-0">
           <CardTitle className="text-xl font-semibold">Receipt AI</CardTitle>
           <CardDescription>
             Scan and analyze your receipts with AI
           </CardDescription>
         </CardHeader>
         <CardContent className="flex justify-center">
-          <button className="flex items-center gap-3 bg-[#2563EB] text-white px-6 py-3 rounded-lg w-sm justify-center cursor-pointer">
+          <Button
+            onClick={() => signIn()}
+            className="flex items-center gap-3 bg-[#2563EB] text-white px-6 py-3 rounded-lg w-sm justify-center cursor-pointer"
+          >
             <MicrosoftLogo />
             Sign in with Microsoft
-          </button>
+          </Button>
         </CardContent>
       </Card>
     </div>
