ccec479e79a8abfa249c10a5b6bb3445797fa333
todor-grigorov <t.grigorov99@gmail.com>
2026-06-16 17:45:31 +0200

    feat: create route group "protected" and move protected pages in it

---
+++ apps/frontend/src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <div className="w-full min-h-screen flex flex-col">
+      <div className="w-full min-h-[calc(100dvh-var(--navbar-height))] flex items-center justify-center">
+        <h1 className="text-2xl font-semibold text-[#111827]">Dashboard</h1>
+      </div>
+    </div>
+  );
+}
--- apps/frontend/src/app/(protected)/layout.tsx
+++ apps/frontend/src/app/(protected)/layout.tsx
@@ -1,14 +1,18 @@
 import Navbar from "@/components/layout/Navbar";
 import AuthGuard from "@/components/providers/AuthGuard";
 
-export default function DashboardPage() {
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <AuthGuard>
       <div className="w-full min-h-screen flex flex-col">
         <Navbar />
-        <div className="w-full min-h-[calc(100dvh-var(--navbar-height))] flex items-center justify-center">
-          <h1 className="text-2xl font-semibold text-[#111827]">Dashboard</h1>
-        </div>
+        <main className="w-full min-h-[calc(100dvh-var(--navbar-height))]">
+          {children}
+        </main>
       </div>
     </AuthGuard>
   );
